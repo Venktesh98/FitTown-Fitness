3f3ae8b767ee0290845a6db25a9ca752ebbed824
Venktesh Soma <venkteshs@cybage.com>
2022-08-29 11:40:07 +0530

    Contact Page Implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Gym/Navbar/Navbar";
 import VideoText from "./components/Gym/LandingPage/VideoText";
-import Contact from "./components/Gym/Contact/Contact";
+import Contact from "./components/Gym/ContactDetails/Contact/Contact";
 import Footer from "./components/Gym/Footer/Footer";
 
 function App() {
