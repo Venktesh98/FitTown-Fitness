897f13b87a2b4b2a76446e52482e54343eb5dcf0
Venktesh Soma <venkteshs@cybage.com>
2022-11-08 18:03:33 +0530

    Added the react toast for the info field

--- src/Hooks/useToast.js
+++ src/Hooks/useToast.js
@@ -33,6 +33,20 @@ export const useToast = () => {
         });
         break;
 
+      case type == "info":
+        toast.info(message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Slide,
+        });
+        break;
+
       default:
         break;
     }
