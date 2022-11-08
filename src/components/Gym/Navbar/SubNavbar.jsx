ea8cc6eee7a4f1d1df96b07ecb847a4cc17a4a34
Venktesh Soma <venkteshs@cybage.com>
2022-11-08 18:02:10 +0530

    Made the links scrollable for the Tablet, Mobile view just like cult.fit for mobile version

--- src/components/Gym/Navbar/SubNavbar.jsx
+++ src/components/Gym/Navbar/SubNavbar.jsx
@@ -7,11 +7,17 @@ const inlineStyles = {
   subHeaderNavLinkItems: {
     color: "white",
     letterSpacing: "1.2px",
+    display: "inline-block",
+    ml: "9%",
+    textAlign: "center",
   },
   comparePlanContainer: {
     // background: "rgb(255 255 255 / 10%)",
     background: "#f5634b",
     borderRadius: "5px",
+    display: "inline-block",
+    ml: "9%",
+    textAlign: "center",
 
     "&:hover": {
       background: "#f5634b",
@@ -26,23 +32,25 @@ const SubNavbar = () => {
   };
   return (
     <div className={styles["sub-header-container"]}>
-      <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Home</Button>
-      <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>About</Button>
-      <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Trainers</Button>
-      <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Gallery</Button>
-      <Button sx={{ ...inlineStyles.comparePlanContainer }}>
-        <div className={styles["compare-plans"]}>
-          <Link
-            // activeClass="active"
-            to="pricing"
-            spy={true}
-            smooth={true}
-            duration={1500}
-          >
-            Compare Plans
-          </Link>
-        </div>
-      </Button>
+      <div className={styles["sub-header-modules"]}>
+        <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Home</Button>
+        <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>About</Button>
+        <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Trainers</Button>
+        <Button sx={{ ...inlineStyles.subHeaderNavLinkItems }}>Gallery</Button>
+        <Button sx={{ ...inlineStyles.comparePlanContainer }}>
+          <div className={styles["compare-plans"]}>
+            <Link
+              // activeClass="active"
+              to="pricing"
+              spy={true}
+              smooth={true}
+              duration={1500}
+            >
+              Compare Plans
+            </Link>
+          </div>
+        </Button>
+      </div>
     </div>
   );
 };
